c39bb3c88c42faa70f973445d65e609818ce2472
下过雨 <2650115317@qq.com>
2026-09-17 07:16:12 +0800

    refactor(activity): defer task claim service

--- tests/test_activity_task_claim_transaction.py
+++ tests/test_activity_task_claim_transaction.py
@@ -11,6 +11,11 @@ from tests.test_db_backend import db_backend
 
 
 class ActivityTaskClaimTransactionTests(unittest.TestCase):
+    def test_activity_service_defers_task_claim_service_construction(self):
+        from nonebot_plugin_xiuxian_2.xiuxian.xiuxian_activity import service
+
+        self.assertIsNone(service._activity_task_claim_service_instance)
+
     def setUp(self):
         self.tmp = tempfile.TemporaryDirectory()
         root = Path(self.tmp.name)
